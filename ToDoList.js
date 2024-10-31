c731d9577e73487ee196558bf4da15ea793eb48b
Asmae <asmaeelhamzaoui77@gmail.com>
2024-10-31 02:27:40 +0100

    ajouter du code javaScript pour le modal

--- ToDoList.js
+++ ToDoList.js
@@ -1,1 +1,12 @@
+const modal=document.getElementById("modal");
+const section1=document.getElementById("section1");
+function ouvrir(){
+    section1.style.display="none";
+    modal.style.display="block";
 
+
+}
+function fermer(){
+    modal.style.display="none";
+    section1.style.display="flex";
+}
