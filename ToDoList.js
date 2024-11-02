362fd2f914ebd76b45d8defb9bdbce3d865ddd25
Asmae <asmaeelhamzaoui77@gmail.com>
2024-11-02 15:56:26 +0100

    ajout du fonction js pour ouvrir le maodal

--- ToDoList.js
+++ ToDoList.js
@@ -1,39 +1,19 @@
-const modal=document.getElementById("modal");
-const section1=document.getElementById("section1");
-const ouvrir=document.getElementById('ouvrir');
-const taskForm=document.getElementById("taskForm");
-//afficher le modal
-ouvrir.addEventListener("click",()=>{
-    section1.style.display="none";
-    modal.style.display="block";
-})
-//masquer le modal
-function fermer(){
-    modal.style.display="none";
-    section1.style.display="flex";
+
+let tasks = [];
+let editingTask = null;
+
+//Ouverture du modal
+function openModal(task = null) {
+  document.getElementById("taskModal").classList.remove("hidden");
+  document.getElementById("modalTitle").innerText = task ? "Modifier la tâche" : "Ajouter une tâche";
+ 
+  if (task) {
+    document.getElementById("title").value = task.title;
+    document.getElementById("description").value = task.description;
+    document.getElementById("date").value = task.date;
+    document.getElementById("priority").value = task.priority;
+    document.querySelector(`input[name='status'][value='${task.status}']`).checked = true;
+    editingTask = task;
+  }
 }
-//testing another way
-const titre=document.getElementById("title").value;
-const description=document.getElementById("description").value;
-const date=document.getElementById("date").value;
-const test=document.getElementById("filmora");
-const status=document.querySelector('input[name="status"]:checked').value;
- test.addEventListener("clisk",()=>{     
-    const task=document.createElement("div");
-        task.classList.add("task");
-        task.innerHTML=`
-             <h4>${titre}</h4>
-             <p>${description}</p>
-             <p>Date:${date}</p>
-        `;
-         //Ajouter la tache à la section correspondante
-    if(status==="todo"){
-        document.getElementById("todoTasks").appendChild(task);
-    }else if(status==="doing"){
-        document.getElementById("doingTasks").appendChild(task);
-    }else if(status==="done"){
-        document.getElementById("doneTasks").appendChild(task);
-    }
- });
-
-
+//Ouverture du modal
